1c378c6ac3242abe48254c61463fc123033977ab
天扬 <90593684+LuTianYanglty@users.noreply.github.com>
2023-11-01 17:47:00 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -14,7 +14,7 @@ export default siteConfig({
   author: '-4°',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Hi，这里是-4°的小小网站！',
 
   // social usernames (optional)
   
